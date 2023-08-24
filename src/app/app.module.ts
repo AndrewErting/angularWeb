c19868ab9621293368035e9c34bb5bdc67a3890c
AndrewErting <122590329+AndrewErting@users.noreply.github.com>
2023-08-24 12:28:39 -0700

    Create new branch for top bar implmentation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatButtonModule } from '@angular/material/button';
+import { TopBarComponent } from './top-bar/top-bar.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
