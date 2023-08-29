793706d2a61f53af43aa21dba82e31fdb34d69f7
AndrewErting <122590329+AndrewErting@users.noreply.github.com>
2023-08-29 13:36:45 -0700

    Created a directive to keep the side-bar exapnd button centered vertically.

--- src/app/center-button-vertical.directive.ts
+++ src/app/center-button-vertical.directive.ts
@@ -1,10 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
 
 @Directive({
   selector: '[appCenterButtonVertical]'
 })
 export class CenterButtonVerticalDirective {
 
-  constructor() { }
+  constructor(
+    private element: ElementRef,
+    private renderer: Renderer2
+  ) { 
+    const sideBarHeight = document.getElementById("side-bar");
+  }
 
 }
