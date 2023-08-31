52f7e8fa7a6af642fbf750150edf269c4832bd7e
AndrewErting <122590329+AndrewErting@users.noreply.github.com>
2023-08-31 13:15:37 -0700

    Made center-button-vertical directive acquire the height of the element it is applied to and include it in the math for dynamic spacing

--- src/app/center-button-vertical.directive.ts
+++ src/app/center-button-vertical.directive.ts
@@ -1,27 +1,27 @@
 import { Directive, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
 
 @Directive({
-  selector: '[appCenterButtonVertical]'
+  selector: '[appCenterButtonVertical]',
 })
 export class CenterButtonVerticalDirective implements AfterViewInit {
-
-  constructor(
-    private element: ElementRef,
-    private renderer: Renderer2
-  ) {
+  constructor(private element: ElementRef, private renderer: Renderer2) {
     var sideBarHeight: any;
     var heightString: string;
-    if(!document.getElementById("side-bar")) {
-      heightString = '400px'
-    }
-    else {
-      sideBarHeight = document.getElementById("side-bar")?.clientHeight;
-      sideBarHeight = (sideBarHeight/2) - 24;
+
+    if (!document.getElementById('side-bar')) {
+      heightString = '400px';
+    } else {
+      sideBarHeight = document.getElementById('side-bar')?.clientHeight;
+      sideBarHeight =
+        sideBarHeight / 2 - this.element.nativeElement.offsetHeight / 2;
       heightString = `${sideBarHeight}px`;
-    };
+    }
 
-    this.renderer.setStyle(this.element.nativeElement, 'margin-top', heightString);
+    this.renderer.setStyle(
+      this.element.nativeElement,
+      'margin-top',
+      heightString
+    );
   }
-  ngAfterViewInit(): void {  }
-
+  ngAfterViewInit(): void {}
 }
