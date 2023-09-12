b83fea1ee0ff04d3b2383597a96b14eb06dcdd69
AndrewErting <122590329+AndrewErting@users.noreply.github.com>
2023-09-12 01:00:40 -0700

    Can now draw on the canvas successfully, just need some way to access the theme colors and apply them to the canvas fillStyle and stroke color.

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -1,13 +1,46 @@
-import { AfterViewChecked, AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-canvas',
-  template: '<canvas #canvas></canvas>',
+  template: '<canvas #canvas id="canvas" class="my-canvas"></canvas>',
   styleUrls: ['./canvas.component.scss']
 })
 export class CanvasComponent implements AfterViewInit {
 
+  @ViewChild('canvas') public canvas!: ElementRef;
+  private htmlCanvas!: HTMLElement | null;
+  private ctx!: CanvasRenderingContext2D | null;
+
   ngAfterViewInit(): void {
+    const canvasEl: HTMLCanvasElement = this.canvas.nativeElement;
+    this.htmlCanvas = document.getElementById('canvas');
+    if(canvasEl && this.htmlCanvas) {
+      this.ctx = canvasEl.getContext('2d');
       
+      this.ctx!.fillStyle = "blue";
+      this.ctx!.fillRect(10, 10, 100, 100);
+
+      this.fillBackground();
+    }
+    else {
+      console.log("Returned Early");
+      return;
+    }
+  }
+
+  paintArray() {
+    this.fillBackground();
+  }
+
+  fillBackground() {
+    this.ctx!.fillRect(10, 10, 100, 100);
+  }
+
+  paintNodes() {
+
+  }
+
+  drawLines() {
+
   }
 }
