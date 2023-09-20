b90640a9ffe50d43e44c8c17ae69ca47d77ae813
AndrewErting <122590329+AndrewErting@users.noreply.github.com>
2023-09-20 10:35:14 -0700

    Changing styling to support different sizes of windows, laptop looked scuffed in comparison to my desktop.

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -1,46 +1,50 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-canvas',
-  template: '<canvas #canvas id="canvas" class="my-canvas"></canvas>',
-  styleUrls: ['./canvas.component.scss']
+    selector: 'app-canvas',
+    template: '<canvas #canvas id="canvas" class="my-canvas"></canvas>',
+    styleUrls: ['./canvas.component.scss'],
 })
 export class CanvasComponent implements AfterViewInit {
+    @ViewChild('canvas') public canvas!: ElementRef;
+    private htmlCanvas!: HTMLElement | null;
+    private ctx!: CanvasRenderingContext2D | null;
 
-  @ViewChild('canvas') public canvas!: ElementRef;
-  private htmlCanvas!: HTMLElement | null;
-  private ctx!: CanvasRenderingContext2D | null;
+    ngAfterViewInit(): void {
+        const canvasEl: HTMLCanvasElement = this.canvas.nativeElement;
+        this.htmlCanvas = document.getElementById('canvas');
+        if (canvasEl && this.htmlCanvas) {
+            this.ctx = canvasEl.getContext('2d');
 
-  ngAfterViewInit(): void {
-    const canvasEl: HTMLCanvasElement = this.canvas.nativeElement;
-    this.htmlCanvas = document.getElementById('canvas');
-    if(canvasEl && this.htmlCanvas) {
-      this.ctx = canvasEl.getContext('2d');
-      
-      this.ctx!.fillStyle = "blue";
-      this.ctx!.fillRect(10, 10, 100, 100);
+            this.ctx!.fillStyle = 'blue';
 
-      this.fillBackground();
+            this.paintArray();
+        } else {
+            console.log('Returned Early');
+            return;
+        }
     }
-    else {
-      console.log("Returned Early");
-      return;
+
+    paintArray() {
+        this.fillBackground();
+        this.paintNodes();
+        this.drawLines();
     }
-  }
 
-  paintArray() {
-    this.fillBackground();
-  }
+    fillBackground() {}
 
-  fillBackground() {
-    this.ctx!.fillRect(10, 10, 100, 100);
-  }
+    paintNodes() {}
 
-  paintNodes() {
+    drawLines() {
+        console.log('Allegedly drawing lines...');
+        this.ctx!.beginPath();
+        this.ctx!.strokeStyle = 'red';
 
-  }
-
-  drawLines() {
-
-  }
+        this.ctx!.beginPath();
+        this.ctx!.moveTo(20, 20);
+        this.ctx!.lineTo(20, 100);
+        this.ctx!.lineTo(70, 100);
+        this.ctx!.strokeStyle = 'red';
+        this.ctx!.stroke();
+    }
 }
